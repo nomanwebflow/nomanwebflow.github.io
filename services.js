3046a754d0959bb96bee3a7a4764d9326c0814e0
thewebflowguy <66731092+nomanwebflow@users.noreply.github.com>
2025-04-13 13:29:29 +0500

    update

--- services.js
+++ services.js
@@ -102,7 +102,7 @@ document.addEventListener("DOMContentLoaded", () => {
       ease: "none",
       scrollTrigger: {
         trigger: "[service-hero-trigger]",
-        start: "top ${getTargetHeight()}px",
+        start: "bottom ${getTargetHeight()}px",
         end: "top top",
         scrub: true,
         invalidateOnRefresh: true, 
